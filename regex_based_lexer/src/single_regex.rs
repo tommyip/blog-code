463a7ec5ae3c8e597e148fc862475879832454ce
Tommy Ip <hkmp7tommy@gmail.com>
2017-08-16 23:32:13 +0800

    regex_based_lexer: Update single_regex to new data structure
    Very expected result from benchmark!

--- regex_based_lexer/src/single_regex.rs
+++ regex_based_lexer/src/single_regex.rs
@@ -1,38 +1,34 @@
-/*
 use super::{Item, Token, Span};
 
 use regex::Regex;
 
-static RULES: [(&str, &str); 11] = [
-    ("Plus", r"\+"),
-    ("Minus", r"\-"),
-    ("Multiply", r"\*"),
-    ("Divide", r"/"),
-    ("Equal", r"="),
-    ("L_Bracket", r"\("),
-    ("R_Bracket", r"\)"),
-    ("Non_Token", r"((\s|\n|\r|\t)+|//.+$)"),
-    ("Integer", r"\d+"),
-    ("Ident", r"\w+"),
-    ("Quote", "^\"[^\n]*?\"")
-];
+static RULES: [(Item, &str); 10] = [(Item::Plus, r"\+"),
+                                    (Item::Minus, r"\-"),
+                                    (Item::Multiply, r"\*"),
+                                    (Item::Divide, r"/"),
+                                    (Item::Equal, r"="),
+                                    (Item::LBracket, r"\("),
+                                    (Item::RBracket, r"\)"),
+                                    (Item::Integer, r"\d+"),
+                                    (Item::Ident, r"\w+"),
+                                    (Item::Quote, "^\"[^\n]*?\"")];
 
 lazy_static! {
+    static ref NON_TOKEN: Regex = Regex::new(r"^(\s|\n)+").unwrap();
     static ref RE: Regex = {
-        let mut re_str =
+        let re_str =
             RULES.iter()
-                .fold(String::new(), |acc, &(rule, re)| {
-                    format!("{}(?P<{}>{})|", acc, rule, re)
+                .fold(String::new(), |acc, &(ref rule, ref re)| {
+                    format!("{}(?P<{:?}>{})|", acc, rule, re)
                 });
-        re_str.pop(); // Remove extra "|"
-        Regex::new(&format!("^({})", re_str)).unwrap()
+        Regex::new(&format!("^({}(?P<Comment>//.+$))", re_str)).unwrap()
     };
 }
 
 #[derive(Debug, PartialEq)]
 struct Lexer<'a> {
     src: &'a str,
-    src_len: usize
+    src_len: usize,
 }
 
 impl<'a> Lexer<'a> {
@@ -48,51 +44,29 @@ impl<'a> Lexer<'a> {
         while pointer < self.src_len {
             let buf = &self.src[pointer..];
 
-            let cap = RE.captures(buf).unwrap();
-            if let Some(mat) = cap.name("Non_Token") {
+            // Skip to token
+            if let Some(mat) = NON_TOKEN.find(buf) {
                 pointer += mat.end() - mat.start();
-            } else if cap.name("Plus").is_some() {
-                Lexer::_process_operator(&mut tokens, Item::Plus, &mut pointer);
-            } else if cap.name("Minus").is_some() {
-                Lexer::_process_operator(&mut tokens, Item::Minus, &mut pointer);
-            } else if cap.name("Multiply").is_some() {
-                Lexer::_process_operator(&mut tokens, Item::Multiply, &mut pointer);
-            } else if cap.name("Divide").is_some() {
-                Lexer::_process_operator(&mut tokens, Item::Divide, &mut pointer);
-            } else if cap.name("Equal").is_some() {
-                Lexer::_process_operator(&mut tokens, Item::Equal, &mut pointer);
-            } else if cap.name("L_Bracket").is_some() {
-                Lexer::_process_operator(&mut tokens, Item::LBracket, &mut pointer);
-            } else if cap.name("R_Bracket").is_some() {
-                Lexer::_process_operator(&mut tokens, Item::RBracket, &mut pointer);
             } else {
-                let token_len;
-                if let Some(mat) = cap.name("Integer") {
-                    token_len = mat.end() - mat.start();
-                    tokens.push(Token(Item::Integer((&self.src[pointer..pointer + token_len])
-                                            .parse::<i32>()
-                                            .unwrap()), Span(pointer, token_len)));
-                } else if let Some(mat) = cap.name("Ident") {
-                    token_len = mat.end() - mat.start();
-                    tokens.push(Token(Item::Ident(&self.src[pointer..pointer + token_len]),
-                                      Span(pointer, token_len)));
-                } else if let Some(mat) = cap.name("Quote") {
-                    token_len = mat.end() - mat.start();
-                    tokens.push(Token(Item::Quote(&self.src[pointer + 1..pointer + token_len - 1]),
-                                      Span(pointer + 1, token_len - 2)));
+                let cap = RE.captures(buf).unwrap();
+                if let Some(mat) = cap.name("Comment") {
+                    pointer += mat.end() - mat.start();
                 } else {
-                    panic!("Unknown token");
+                    for &(ref rule, _) in RULES.iter() {
+                        if let Some(mat) = cap.name(&format!("{:?}", rule)) {
+                            let token_len = mat.end() - mat.start();
+                            tokens.push(Token(rule.clone(),
+                                              &self.src[pointer..pointer + token_len],
+                                              Span(pointer, pointer + token_len)));
+                            pointer += token_len;
+                            break;
+                        }
+                    }
                 }
-                pointer += token_len;
             }
         }
 
         tokens
-    }
-
-    fn _process_operator(tokens: &mut Vec<Token<'a>>, op: Item<'a>, pointer: &mut usize) {
-        (*tokens).push(Token(op, Span(*pointer, 1)));
-        *pointer += 1;
     }
 }
 
@@ -109,19 +83,18 @@ mod tests {
         let src = "person = (head + body) * \"mind\" + 42";
         let lexer = Lexer::new(src);
 
-        assert_eq!(lexer.lex(), vec![
-            Token(Item::Ident("person"), Span(0, 6)),
-            Token(Item::Equal,           Span(7, 1)),
-            Token(Item::LBracket,        Span(9, 1)),
-            Token(Item::Ident("head"),   Span(10, 4)),
-            Token(Item::Plus,            Span(15, 1)),
-            Token(Item::Ident("body"),   Span(17, 4)),
-            Token(Item::RBracket,        Span(21, 1)),
-            Token(Item::Multiply,        Span(23, 1)),
-            Token(Item::Quote("mind"),   Span(26, 4)),
-            Token(Item::Plus,            Span(32, 1)),
-            Token(Item::Integer(42),     Span(34, 2)),
-        ]);
+        assert_eq!(lexer.lex(),
+                   vec![Token(Item::Ident, "person", Span(0, 6)),
+                        Token(Item::Equal, "=", Span(7, 8)),
+                        Token(Item::LBracket, "(", Span(9, 10)),
+                        Token(Item::Ident, "head", Span(10, 14)),
+                        Token(Item::Plus, "+", Span(15, 16)),
+                        Token(Item::Ident, "body", Span(17, 21)),
+                        Token(Item::RBracket, ")", Span(21, 22)),
+                        Token(Item::Multiply, "*", Span(23, 24)),
+                        Token(Item::Quote, "\"mind\"", Span(25, 31)),
+                        Token(Item::Plus, "+", Span(32, 33)),
+                        Token(Item::Integer, "42", Span(34, 36))]);
     }
 
     #[bench]
@@ -136,9 +109,6 @@ mod tests {
             };
         }
         let lexer = Lexer::new(&SRC);
-        b.iter(|| {
-            let _ = lexer.lex();
-        });
+        b.iter(|| { let _ = lexer.lex(); });
     }
 }
-*/
